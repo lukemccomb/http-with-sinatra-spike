aef2625c5ce4f568be02aab7d1eb0b340105b982
Luke McComb <lucasmccomb@gmail.com>
2014-06-26 14:58:10 -0600

    created dynamic functionality with different photos and music, only had to write one line of html for 3+ items on each page

--- app.rb
+++ app.rb
@@ -4,7 +4,20 @@ class MyApp < Sinatra::Application
 
   def initialize
     super
-    @items = [1]
+    @photos = ["http://lukemccomb.com/images/abstract1.jpg",
+               "http://lukemccomb.com/images/outdoors1.jpg",
+               "http://lukemccomb.com/images/abstract2.jpg",
+               "http://lukemccomb.com/images/abstract6.jpg",
+               "http://lukemccomb.com/images/outdoors12.jpg",
+               "http://lukemccomb.com/images/outdoors13.jpg",
+               "http://lukemccomb.com/images/outdoors15.jpg",
+               "http://lukemccomb.com/images/people1.jpg",
+               "http://lukemccomb.com/images/objects1.jpg",
+               "http://lukemccomb.com/images/events1.jpg",
+    ]
+    @music = ["https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/156153650&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true",
+    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/155143944&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true",
+    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/150593597&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"]
   end
 
   get "/" do
@@ -12,8 +25,13 @@ class MyApp < Sinatra::Application
   end
 
   get "/music" do
-    erb :music
+    erb :music, :locals => {:music => @music}
   end
+
+  # get "/music/:identifier" do
+  #   identifier = params[:identifier]
+  #   "You found my jam: #{identifier}"
+  # end
 
   run! if app_file == $0
 end
